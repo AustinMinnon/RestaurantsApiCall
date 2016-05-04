c85b6401f6cd276713069b0e608a23af831e77d3
Ausitn Minnon and chris houghton <whi7ee@gmail.com>
2016-05-04 09:53:25 -0700

    adds login authentication

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -29,12 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton ;
     @Bind(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
 
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
@@ -44,11 +46,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public boolean onCreateOptionsMenu (Menu menu){
             MenuInflater inflater = getMenuInflater();
             inflater.inflate(R.menu.menu_search, menu);
+            inflater.inflate(R.menu.menu_main, menu);
             return true;
         }
 
         @Override
         public boolean onOptionsItemSelected (MenuItem item){
+           int id = item.getItemId();
+            if (id == R.id.action_logout){
+                logout();
+                return true;
+            }
             return super.onOptionsItemSelected(item);
         }
 
@@ -63,4 +71,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
             }
         }
+    protected void logout() {
+        mFirebaseRef.unauth();
+        takeUserToLoginScreenOnUnAuth();
+    }
+
+    private void takeUserToLoginScreenOnUnAuth(){
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
     }
