d83633ad8ec93baa8aebd6e0981cf8ac1ac15999
Ausitn Minnon and chris houghton <Guest@epicodus-82.local>
2016-05-04 11:50:08 -0700

    add more login function

--- app/src/main/java/com/example/guest/chitchat/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/chitchat/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.guest.chitchat;
+package com.example.guest.chitchat.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.guest.chitchat.R;
 
 public class MainActivity extends AppCompatActivity {
 
